b57dd1fe982b03da54c3cf30381c00f5f86caecd
Ari Kauppi <ari.kauppi@helsinki.fi>
2021-03-30 23:49:25 +0300

    v2 tehtavia

---
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -0,0 +1,13 @@
+import unittest
+from maksukortti import Maksukortti
+
+class TestMaksukortti(unittest.TestCase):
+    def setUp(self):
+        self.maksukortti = Maksukortti(1000)
+
+    def test_luotu_kortti_on_olemassa(self):
+        self.assertNotEqual(self.maksukortti, None)
+    
+    def test_saldo_alussa_oikein(self):
+        print(self.maksukortti.__str__())
+        self.assertEqual(str(self.maksukortti), "saldo: 10")
